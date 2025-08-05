0651c280af83246dbffe7ae2b6fa68856afc38a1
bjee19 <139261241+bjee19@users.noreply.github.com>
2025-08-05 16:29:52 -0700

    Adjust logging when resolving endpoints (#3690)
    Adjust logging when resolving endpoints so that it no longer sends an error message but is instead a debug log message. Also, add a debug log message upon successful resolving of endpoints.

--- internal/controller/state/dataplane/configuration.go
+++ internal/controller/state/dataplane/configuration.go
@@ -781,7 +781,9 @@ func buildUpstream(
 	eps, err := svcResolver.Resolve(ctx, logger, br.SvcNsName, br.ServicePort, allowedAddressType)
 	if err != nil {
 		errMsg = err.Error()
-		logger.Error(err, "failed to resolve endpoints", "service", br.SvcNsName)
+		logger.V(1).Info("failed to resolve endpoints, endpoints may not be ready", "error", errMsg, "service", br.SvcNsName)
+	} else {
+		logger.V(1).Info("successfully resolved endpoints", "service", br.SvcNsName)
 	}
 
 	var upstreamPolicies []policies.Policy
